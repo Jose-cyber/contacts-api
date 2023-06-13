97a4bbc7e2053dc6613fa2cd8fc62c3ca127c165
jose costa <jose.costa@2rpnet.com>
2023-06-13 13:02:35 -0300

    criado rotas, criado controllers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+const logger = require('./configs/logger.js');
 const app = require('./infra/server.js');
 require('dotenv').config()
 
-app.listen(process.env.PORT || 8000, () => {
-   console.log('Server runing!')
+app.listen(process.env.PORT || 8080, () => {
+   logger.info("Server runing on: http://0.0.0.0:" + process.env.PORT || 8080)
+   logger.info('server runing in mode: '+ process.env.NODE_ENV)
 })
--- src/infra/server.js
+++ src/infra/server.js
@@ -1,32 +1,10 @@
 const express = require('express');
-const db = require('../database/conection.js')
+const healthAndCheckingRoutes = require('../routes/healthAndChecking.js')
+const contactsRoutes = require('../routes/contactsRoutes.js')
 const app = express();
 
 
-app.get('/', (req, res) => {
-    res.send({message: 'contacts api'})
-});
-
-app.post('/api/v1/contacts/contact', (req, res) => {
-
-});
-
-app.get('/api/v1/contacts/list', (req, res) => {
-
-});
-
-app.get('/api/v1/contacts/health', (req, res) =>{
-    db.raw("SELECT 1").then(() => {
-        res.json("UP").status(200);
-    })
-    .catch((e) => {
-        res.json("DOWN").status(500);
-        console.error(e);
-    });
-});
-
-app.get('/api/v1/contacts/docs', (req, res) =>{
-
-});
+app.use(healthAndCheckingRoutes);
+app.use(contactsRoutes);
 
 module.exports = app;
